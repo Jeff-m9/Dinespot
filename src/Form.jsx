3076205fed9d83e6dfd3591fefb3e481642494af
Bildad Ereggae <bildadereggae@gmail.com>
2025-04-22 09:47:37 +0300

    Form wip

---
+++ src/Form.jsx
@@ -0,0 +1,15 @@
+function Form() {
+  return (
+    <div>
+          <form action="">
+              <input type="text" name="name" placeholder="Enter the name" />
+              <input type="text" name="cuisine" placeholder="cuisine" />
+              <input type="text" name="location" placeholder="Enter location" />
+              <input type="text" name="menu" placeholder="Menu" />
+              <input type="image" name="image" placeholder="Image"/>
+      </form>
+    </div>
+  );
+}
+
+export default Form;

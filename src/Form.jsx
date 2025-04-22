6194c9708cdc808c80b97f0d7fdcba2d0a03ba1e
Bildad Ereggae <bildadereggae@gmail.com>
2025-04-22 10:46:19 +0300

    handle change & handle submit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,73 @@
+import { useState } from "react";
+
+const initialValues = {
+  name: "",
+  cuisine: "",
+  location: "",
+  menu: "",
+  image: "",
+};
+
 function Form() {
+  const [formData, setFormData] = useState(initialValues);
+
+  // to handle the inputs
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  // to handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      console.log("form submitted");
+  };
+
   return (
     <div>
-          <form action="">
-              <input type="text" name="name" placeholder="Enter the name" />
-              <input type="text" name="cuisine" placeholder="cuisine" />
-              <input type="text" name="location" placeholder="Enter location" />
-              <input type="text" name="menu" placeholder="Menu" />
-              <input type="image" name="image" placeholder="Image"/>
+      <form onSubmit={handleSubmit} onChange={handleChange}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter the name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="cuisine"
+          placeholder="cuisine"
+          value={formData.cuisine}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="location"
+          placeholder="Enter location"
+          value={formData.location}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="menu"
+          placeholder="Menu"
+          value={formData.menu}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image"
+          value={formData.image}
+          onChange={handleChange}
+        />
+        <button type="submit">Add</button>
       </form>
     </div>
   );

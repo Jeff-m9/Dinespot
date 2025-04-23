cbb4242f0fa23494b58a12faa9650d2b4f20458b
+ <+>
2025-04-23 21:16:22 +0800

    header+footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white text-center text-black">
+            <p> @2025  DINESPOT  All Rights Reserved</p>
+        </footer>
+
+    );
+};
+export default Footer;
